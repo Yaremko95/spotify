419e3441ff819a6a4e3c62c527d54a17d267d226
tetiana <tetianayaremko@gmail.com>
2020-05-22 15:02:14 +0200

    serach

--- js/search.js
+++ js/search.js
@@ -1,4 +1,8 @@
-fetch(`https://deezerdevs-deezer.p.rapidapi.com/search?q=money`, {
+
+function searchSongs()
+ {
+  let input =document.querySelector('.search-container .form-control').value.toLowerCase()
+   fetch(`https://deezerdevs-deezer.p.rapidapi.com/search?q=money`, {
 		"method": "GET",
 		"headers": {
 			"x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
@@ -8,18 +12,44 @@ fetch(`https://deezerdevs-deezer.p.rapidapi.com/search?q=money`, {
 		.then(response => response.json()).then(parsedJSON => { 
         let data=parsedJSON.data
         let section = document.querySelector('.search-results-section')
+        let containers= document.querySelectorAll('.search-results-section .container-fluid')
+        let songsSection = document.querySelector('.songs-search')
+        let defaultSearch = document.querySelector('.search-default')
+        
+
         let tracksQuerry = data;
         const albumQuerry = data.filter(element => 
             element.album.title.toLowerCase().includes(`money`))
-            section.innerHTML="";
-            albumQuerry.forEach(element => {
-                let content = `<div class="col">
-                <div class="d-block my-5 text-center">
-                  <img src="./assets/q1.jpg" class="img-fluid rounded-circle" alt="">
-                  <span class="text-light display-5">Artist Name</span>
-                </div>
-              </div>`
-                let div
+            defaultSearch.remove()
+            containers.forEach(element => {
+              element.classList.toggle('d-none')
+            });
+            // let loadedBooks=[]
+            //      if(input!='' && input.length>=3) {
+            //        loadedBooks=  data.filter(book=>book.title.toLowerCase().includes(input))
+            //        booksContainer.innerHTML='';
+            //      } else {
+            //          loadedBooks =data
+            //          booksContainer.innerHTML='';
+            //      }
+           
+
+            tracksQuerry.forEach((element, index) => {
+                if(index < 8) {
+                        let content = `<div class="col mb-3">
+                                  <div class="d-flex">
+                                    <img src="${element.album.cover_small}" alt="" style="max-height: 5rem;">
+                                    <div class="d-flex flex-column pl-2">
+                                      <span>${element.title}</span>
+                                      <a href="">
+                                        <small>${element.artist.name}</small>
+                                      </a>
+                                    </div>
+                                  </div>
+                                </div>`
+                songsSection.innerHTML += content
+                }
+          
             })
         const artistQuerry = data.filter(element => 
             element.artist.name.toLowerCase().includes(`money`))
@@ -28,5 +58,5 @@ fetch(`https://deezerdevs-deezer.p.rapidapi.com/search?q=money`, {
             })
             console.log(tracksQuerry,albumQuerry,artistQuerry)
     })   
-    
+  }
         
