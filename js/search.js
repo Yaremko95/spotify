20a04a48e2851b180739f72508581440cfbf6faf
tetiana <tetianayaremko@gmail.com>
2020-05-24 01:16:19 +0200

    update

--- js/search.js
+++ js/search.js
@@ -1,36 +1,36 @@
 
 
-function seeMoreSongs (){
+// function seeMoreSongs (){
 
-    console.log(data)
-    let main = document.querySelector('.search-results-section'),
-    containers = document.querySelectorAll('.search-results-section .container-fluid')
-    containers.forEach((element) => {
-      element.classList.add("d-none");
-    });
-    divRow=document.createElement('div');
-    divRow.className="row pr-5"
+//     console.log(data)
+//     let main = document.querySelector('.search-results-section'),
+//     containers = document.querySelectorAll('.search-results-section .container-fluid')
+//     containers.forEach((element) => {
+//       element.classList.add("d-none");
+//     });
+//     divRow=document.createElement('div');
+//     divRow.className="row pr-5"
    
    
-      trackquerry.forEach(element => {
+//       trackquerry.forEach(element => {
 
-    let content=`
-                <ul class="w-100">
-                <li class="d-flex  mb-3 justify-content-between w-100  border-bottom border-top border-secondary py-3  " style="cursor:pointer">
-                  <div class="d-flex flex-column">
-                    <span class='song-title'><i class="fa fa-music mr-3" ></i>${element.title}</span>
+//     let content=`
+//                 <ul class="w-100">
+//                 <li class="d-flex  mb-3 justify-content-between w-100  border-bottom border-top border-secondary py-3  " style="cursor:pointer">
+//                   <div class="d-flex flex-column">
+//                     <span class='song-title'><i class="fa fa-music mr-3" ></i>${element.title}</span>
                     
-                  </div>
-                    <span class="mr-3">${element.duration/100}</span>
-                </li>
-              </ul>`
-  divRow.innerHTML+=content
+//                   </div>
+//                     <span class="mr-3">${element.duration/100}</span>
+//                 </li>
+//               </ul>`
+//   divRow.innerHTML+=content
 
   
 
-  })
-    main.appendChild(divRow)
+//   })
+//     main.appendChild(divRow)
 
 
-  }
+//   }
  
