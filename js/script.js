2a3ab9792f1c31b1df1612caed4225c17a48fd94
tetiana <tetianayaremko@gmail.com>
2020-05-24 03:17:34 +0200

    search all songs

--- js/script.js
+++ js/script.js
@@ -29,6 +29,8 @@ let filteredArtists =[]
   containers.forEach((element) => {
     element.classList.remove("d-none");
   });
+  let seeAllTracksSection=document.querySelector('.see-all-songs')
+  seeAllTracksSection.classList.add('d-none')
 
       filteredArtists=[];
       filteredAlbums=[]
@@ -159,4 +161,71 @@ const fetchArtists = (value, index) => {
       });
     }
     })
+}
+
+const seeAllTracks =() => {
+  let searchInput = document.getElementById("search-artist");
+  let value =searchInput.value;
+  let containers = document.querySelectorAll(
+    ".search-results-section .container-fluid"
+  );
+ 
+  containers.forEach((element) => {
+    element.classList.add("d-none");
+  });
+  let seeAllTracksSection=document.querySelector('.see-all-songs')
+  seeAllTracksSection.classList.remove('d-none')
+  let tableBody = document.querySelector('.see-all-songs tbody')
+  tableBody.innerHTML=""
+  filteredTracks =[]
+  fetchAllTracks(value, index=0)
+
+}
+
+const fetchAllTracks =(value, index) => {
+  let tableBody = document.querySelector('.see-all-songs tbody')
+   fetch(`https://deezerdevs-deezer.p.rapidapi.com/search/track/title?q=${value}&index=${index}`, {
+        method: "GET",
+        headers: {
+          "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+          "x-rapidapi-key": "84d2e1bc2amsh0bcbc81dd32f547p1526bajsncbac98b453bc",
+        },
+      })
+        .then((response) => response.json())
+        .then((json) => {
+          console.log(json.total, index)
+          // filteredTracks =_.concat(filteredTracks, json.data.filter(track=>track.title.toLowerCase().includes(`${value}`)))
+          filteredTracks = json.data.filter(track=>track.title.toLowerCase().includes(`${value}`))
+          console.log(filteredTracks)
+          if(index<json.total) {
+            fetchAllTracks(value, index+=25)
+          // } else {
+            _.uniqBy(filteredTracks, "title", "artist.name").forEach(track => {
+              let content =`
+                        <tr>
+                          <th scope="row" style="width:30px"></th>
+                          <td ><i class="fa fa-music mr-3" ></i></td>
+                          <td>${track.title}</td>
+                          <td>${track.artist.name}</td>
+                          <td>${track.album.title}</td>
+                          <td>${(track.duration/60).toFixed(2)}</td>
+                        </tr>
+              `;
+              tableBody.innerHTML+=content;
+            });
+            let tableRows=document.querySelectorAll('.see-all-songs tr')
+            let tds=document.querySelectorAll('.see-all-songs tbody tr th')
+            if(tds.length>0) {
+               tableRows.forEach((tableRow, index) => {
+              tableRow.addEventListener('mouseover', function() {
+               tds[index-1].innerHTML = ` <i   class="fa fa-play-circle "></i>`
+              })
+              tableRow.addEventListener('mouseout', function() {
+                tds[index-1].innerHTML = ``
+               })
+            });
+            }
+           
+          }
+        })
 }
